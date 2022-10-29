509144b3a6b7539a3592f8b0a5006d3a1282cb42
Bianca Prado <100374298+projectspradobianca@users.noreply.github.com>
2022-10-29 11:01:19 -0300

    Delete Cliente.java

--- Cliente.java
+++
@@ -1,8 +0,0 @@
-package com.senac.loja;
-
-public class Cliente extends Pessoa {
-    private String telefone;
-    public Cliente(String cpf, String email, String nome) {
-        super(cpf, email, nome);
-    }
-}
